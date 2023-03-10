6851beb606711249dd99b30885874c2a33c7f1b6
Ilyakok <ilya.kachelovskiy@gmail.com>
2023-03-10 16:09:04 +0300

    003: сделал задержку в 3 сек перед переходом на главный экран

--- lib/presentation/screens/splash/splash_screen.dart
+++ lib/presentation/screens/splash/splash_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:group/core/constants/asset_provider.dart';
 import 'package:group/core/constants/string_provider.dart';
 import 'package:group/core/constants/ui_kit_colors.dart';
 import 'package:group/core/constants/ui_kit_text_style.dart';
+import 'package:group/presentation/screens/main_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -13,6 +16,12 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  initState() {
+    super.initState();
+    _navigateToMainScreen();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
@@ -22,12 +31,23 @@ class _SplashScreenState extends State<SplashScreen> {
             Text(StringProvider.splashScreenTitle, style: UiKitTextStyle.title),
             Padding(
               padding: const EdgeInsets.only(top: 40, bottom: 40),
-              child: Center(child: Image.asset(AssetProvider.splashScreen)),
+              child: Center(
+                child: Image.asset(AssetProvider.splashScreen),
+              ),
             ),
             const CircularProgressIndicator(color: UiKitColors.red),
           ],
         ),
       ),
     );
+  }
+
+  /// Переход на Гланую страницу после паузы в 3 сек
+  Future<void> _navigateToMainScreen() async {
+    await Future.delayed(const Duration(seconds: 3));
+    if (context.mounted) {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const MainScreen()));
+    }
   }
 }
