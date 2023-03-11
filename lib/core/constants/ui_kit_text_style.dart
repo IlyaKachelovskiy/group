db2bc2bbc41b44981a8c33e4b005872b88320b54
Ilyakok <ilya.kachelovskiy@gmail.com>
2023-03-11 19:36:47 +0300

    004: обновил стили, цвета

--- lib/core/constants/ui_kit_text_style.dart
+++ lib/core/constants/ui_kit_text_style.dart
@@ -2,10 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:group/core/constants/ui_kit_colors.dart';
 
 class UiKitTextStyle {
-  static TextStyle get title => const TextStyle(
-        fontSize: 32,
+  static TextStyle get splashScreenTitle => const TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Mulish',
+    color: UiKitColors.black,
+  );
+
+  static TextStyle get pageTitle => const TextStyle(
+        fontSize: 20,
         fontWeight: FontWeight.w700,
         fontFamily: 'Mulish',
-        color: UiKitColors.mainDark,
+        color: UiKitColors.black,
       );
+
+  static TextStyle get description => const TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Mulish',
+    color: UiKitColors.gray,
+  );
+
 }
