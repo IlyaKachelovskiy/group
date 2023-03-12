0e7427de170343c885c06748abb5594158c08ed9
Ilyakok <ilya.kachelovskiy@gmail.com>
2023-03-12 21:59:47 +0300

    007: обновил стринг провайдер для экрана профиля

--- lib/core/constants/string_provider.dart
+++ lib/core/constants/string_provider.dart
@@ -7,4 +7,15 @@ class StringProvider {
 
   ///Profile screen
   static String get profileScreenTitle => 'Профиль';
+
+  static String get profileScreenHintText =>
+      'Введите своё имя и при желании добавьте фото профиля';
+
+  static String get profileScreenFirstName => 'Имя';
+
+  static String get profileScreenLastName => 'Фамилия';
+
+  static String get profileScreenTitleEmail => 'Email';
+
+  static String get profileScreenTitlePhoneNumber => 'Номер телефона';
 }
